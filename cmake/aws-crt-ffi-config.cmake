17a854a708feec7727b86dc383b5312a8b17ddf0
Justin Boswell <boswej@amazon.com>
2021-01-13 19:51:13 +0000

    added aws-checksums and cmake config

---
+++ cmake/aws-crt-ffi-config.cmake
@@ -0,0 +1,16 @@
+include(CMakeFindDependencyMacro)
+
+if (UNIX AND NOT APPLE)
+    find_dependency(s2n)
+endif()
+
+find_dependency(aws-c-common)
+find_dependency(aws-c-cal)
+find_dependency(aws-c-io)
+find_dependency(aws-c-http)
+find_dependency(aws-c-auth)
+find_dependency(aws-c-event-stream)
+find_dependency(aws-c-compression)
+find_dependency(aws-checksums)
+
+include(${CMAKE_CURRENT_LIST_DIR}/shared/@PROJECT_NAME@-targets.cmake)
