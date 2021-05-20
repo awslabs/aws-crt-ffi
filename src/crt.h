378593802fa7c25c49ab94085fce1318487f3dc2
Justin Boswell <boswej@amazon.com>
2021-05-19 17:10:58 -0700

    Fixed clang-format's wagon

--- src/crt.h
+++ src/crt.h
@@ -5,8 +5,10 @@
  * SPDX-License-Identifier: Apache-2.0.
  */
 
-#include <aws/common/common.h>
+/* clang-format off */
+#include <aws/common/common.h> /* must be present so api.h knows about inttypes and allocators */
 #include "api.h"
+/* clang-format on */
 
 /* Utility functions for use within this library */
 
