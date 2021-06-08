91cdb37398c154e1e5c536c553adbe7ca972aa80
Itamar Levy-Or <ilevyor@amazon.com>
2021-06-08 11:35:00 -0400

    formatting

--- src/crc.c
+++ src/crc.c
@@ -2,8 +2,8 @@
  * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0.
  */
-#include "crt.h"
 #include "aws/checksums/crc.h"
+#include "crt.h"
 
 uint32_t aws_crt_crc32(const uint8_t *input, int length, uint32_t previous) {
     return aws_checksums_crc32(input, length, previous);
