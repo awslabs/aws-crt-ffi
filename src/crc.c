58ccbe43eea8117e2ade3ea665bc2c330ede7b01
Itamar Levy-Or <ilevyor@amazon.com>
2021-06-08 13:01:24 -0400

    build fix

--- src/crc.c
+++ src/crc.c
@@ -5,7 +5,7 @@
 
 #include "crt.h"
 #include <aws/checksums/crc.h>
-//
+
 uint32_t aws_crt_crc32(const uint8_t *input, int length, uint32_t previous) {
     return aws_checksums_crc32(input, length, previous);
 }
