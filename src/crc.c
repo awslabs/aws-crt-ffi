ea0da659c49834845133f7335a0e8c98eab18345
Itamar Levy-Or <ilevyor@amazon.com>
2021-06-14 09:55:50 -0400

    crc_common

--- src/crc.c
+++ src/crc.c
@@ -4,12 +4,29 @@
  */
 
 #include "crt.h"
+#include "limits.h"
 #include <aws/checksums/crc.h>
 
-uint32_t aws_crt_crc32(const uint8_t *input, int length, uint32_t previous) {
-    return aws_checksums_crc32(input, length, previous);
+uint32_t crc_common(
+    uint32_t (*checksum_fn)(const uint8_t *, int, uint32_t),
+    const uint8_t *buffer,
+    uint32_t length,
+    uint32_t previous) {
+
+    uint32_t val = previous;
+    while (length > INT_MAX) {
+        val = checksum_fn(buffer, INT_MAX, val);
+        buffer += (size_t)INT_MAX;
+        length -= (size_t)INT_MAX;
+    }
+    val = checksum_fn(buffer, (int)length, val);
+    return val;
 }
 
-uint32_t aws_crt_crc32c(const uint8_t *input, int length, uint32_t previous) {
-    return aws_checksums_crc32c(input, length, previous);
+uint32_t aws_crt_crc32(const uint8_t *input, uint32_t length, uint32_t previous) {
+    return crc_common(aws_checksums_crc32, input, length, previous);
+}
+
+uint32_t aws_crt_crc32c(const uint8_t *input, uint32_t length, uint32_t previous) {
+    return crc_common(aws_checksums_crc32, input, length, previous);
 }
