3aa32ae943f9a7b22b8d29a17068c731469c77ca
Benjamin <tu.benjamin1115@gmail.com>
2021-06-25 12:07:47 -0700

    refactor http (#13)
    * refactor http blob/add http input_stream
    
    * delete extra stuff
    
    Co-authored-by: Tu <bnjamit@88665a0c58cd.ant.amazon.com>

--- src/api.h
+++ src/api.h
@@ -106,22 +106,25 @@ AWS_CRT_API int aws_crt_input_stream_get_status(aws_crt_input_stream *stream, aw
 AWS_CRT_API int aws_crt_input_stream_get_length(aws_crt_input_stream *stream, int64_t *length);
 
 /* HTTP */
+typedef struct aws_crt_buf {
+    uint8_t *blob;
+    size_t length;
+} aws_crt_buf;
 typedef struct _aws_crt_http_headers aws_crt_http_headers;
 AWS_CRT_API aws_crt_http_headers *aws_crt_http_headers_new_from_blob(const uint8_t *blob, size_t blob_length);
 AWS_CRT_API aws_crt_http_headers *aws_crt_http_headers_acquire(aws_crt_http_headers *headers);
 AWS_CRT_API void aws_crt_http_headers_release(aws_crt_http_headers *headers);
 AWS_CRT_API void aws_crt_http_headers_to_blob(
     const aws_crt_http_headers *headers,
-    uint8_t **out_blob,
-    size_t *out_blob_length);
+    aws_crt_buf *out_blob);
 
 typedef struct _aws_crt_http_message aws_crt_http_message;
 AWS_CRT_API aws_crt_http_message *aws_crt_http_message_new_from_blob(const uint8_t *blob, size_t blob_length);
+AWS_CRT_API void aws_crt_http_message_set_body_stream(aws_crt_http_message *message, aws_crt_input_stream *body_stream);
 AWS_CRT_API void aws_crt_http_message_release(aws_crt_http_message *message);
 AWS_CRT_API void aws_crt_http_message_to_blob(
     const aws_crt_http_message *message,
-    uint8_t **out_blob,
-    size_t *out_blob_length);
+    aws_crt_buf *out_blob);
 
 /* Auth */
 typedef struct aws_credentials aws_crt_credentials;
