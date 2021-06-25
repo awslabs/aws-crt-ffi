9f855a848b474a449565f631f93797d948dae5e0
Tu <bnjamit@88665a0c58cd.ant.amazon.com>
2021-06-25 12:45:46 -0700

    format

--- src/api.h
+++ src/api.h
@@ -114,17 +114,13 @@ typedef struct _aws_crt_http_headers aws_crt_http_headers;
 AWS_CRT_API aws_crt_http_headers *aws_crt_http_headers_new_from_blob(const uint8_t *blob, size_t blob_length);
 AWS_CRT_API aws_crt_http_headers *aws_crt_http_headers_acquire(aws_crt_http_headers *headers);
 AWS_CRT_API void aws_crt_http_headers_release(aws_crt_http_headers *headers);
-AWS_CRT_API void aws_crt_http_headers_to_blob(
-    const aws_crt_http_headers *headers,
-    aws_crt_buf *out_blob);
+AWS_CRT_API void aws_crt_http_headers_to_blob(const aws_crt_http_headers *headers, aws_crt_buf *out_blob);
 
 typedef struct _aws_crt_http_message aws_crt_http_message;
 AWS_CRT_API aws_crt_http_message *aws_crt_http_message_new_from_blob(const uint8_t *blob, size_t blob_length);
 AWS_CRT_API void aws_crt_http_message_set_body_stream(aws_crt_http_message *message, aws_crt_input_stream *body_stream);
 AWS_CRT_API void aws_crt_http_message_release(aws_crt_http_message *message);
-AWS_CRT_API void aws_crt_http_message_to_blob(
-    const aws_crt_http_message *message,
-    aws_crt_buf *out_blob);
+AWS_CRT_API void aws_crt_http_message_to_blob(const aws_crt_http_message *message, aws_crt_buf *out_blob);
 
 /* Auth */
 typedef struct aws_credentials aws_crt_credentials;
