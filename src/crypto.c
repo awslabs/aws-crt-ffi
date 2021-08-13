c9dab6751f5297c1bd945650eba392fb07add715
Justin Boswell <boswej@amazon.com>
2021-08-13 23:38:58 +0000

    Separated out crypto init/shutdown

--- src/crypto.c
+++ src/crypto.c
@@ -8,7 +8,9 @@
 void aws_crt_crypto_share(void) {
     /* Prevent s2n from initializing or de-initializing crypto */
     s2n_crypto_disable_init();
+}
 
+void init_crypto(void) {
     /*
      * OpenSSL prior to 1.1.x has idempotency issues with initialization and shutdown.
      * We initialize it minimally ourselves here, since s2n has been told not to.
@@ -18,10 +20,16 @@ void aws_crt_crypto_share(void) {
     OPENSSL_add_all_algorithms();
 #    else
     OPENSSL_init_crypto(
-        OPENSSL_INIT_LOAD_CRYPTO_STRINGS | OPENSSL_INIT_ADD_ALL_CIPHERS | OPENSSL_INIT_ADD_ALL_DIGESTS, NULL);
+        OPENSSL_INIT_LOAD_CRYPTO_STRINGS | OPENSSL_INIT_ADD_ALL_CIPHERS | OPENSSL_INIT_ADD_ALL_DIGESTS | OPENSSL_INIT_NO_ATEXIT, NULL);
 #    endif
+}
+
+void shutdown_crypto(void) {
+
 }
 
 #else
 void aws_crt_crypto_share(void) {}
+void init_crypto(void) {}
+void shutdown_crypto(void) {}
 #endif
