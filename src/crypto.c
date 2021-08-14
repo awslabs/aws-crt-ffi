e6e420c47dba100b7c869440ffe87596ceef158e
Justin Boswell <boswej@amazon.com>
2021-08-14 00:21:06 +0000

    Try to make the most of whatever version of openssl we find

--- src/crypto.c
+++ src/crypto.c
@@ -11,8 +11,8 @@ void aws_crt_crypto_share(void) {
     s2n_crypto_disable_init();
 }
 
-#define AWS_OPENSSL_VERSION_AT_LEAST(major, minor, fix) \
-    (OPENSSL_VERSION_NUMBER >= ((major << 28) + (minor << 20) + (fix << 12)))
+#    define AWS_OPENSSL_VERSION_AT_LEAST(major, minor, fix)                                                            \
+        (OPENSSL_VERSION_NUMBER >= ((major << 28) + (minor << 20) + (fix << 12)))
 
 void init_crypto(void) {
     /*
@@ -20,18 +20,21 @@ void init_crypto(void) {
      * We initialize it minimally ourselves here, since s2n has been told not to.
      * Cleanup is handled by OpenSSL's atexit handler
      */
-#    if !AWS_OPENSSL_VERSION_AT_LEAST(1, 1, 0)
-    OpenSSL_add_all_algorithms();
-#    else
+#    if AWS_OPENSSL_VERSION_AT_LEAST(1, 1, 1)
     OPENSSL_init_crypto(
+        OPENSSL_INIT_LOAD_CRYPTO_STRINGS | OPENSSL_INIT_ADD_ALL_CIPHERS | OPENSSL_INIT_ADD_ALL_DIGESTS |
+            OPENSSL_INIT_NO_ATEXIT,
+        NULL);
+        #elif AWS_OPENSSL_VERSION_AT_LEAST(1, 1, 0)
+        OPENSSL_init_crypto(
         OPENSSL_INIT_LOAD_CRYPTO_STRINGS | OPENSSL_INIT_ADD_ALL_CIPHERS | OPENSSL_INIT_ADD_ALL_DIGESTS,
         NULL);
+#    else
+    OpenSSL_add_all_algorithms();
 #    endif
 }
 
-void shutdown_crypto(void) {
-
-}
+void shutdown_crypto(void) {}
 
 #else
 void aws_crt_crypto_share(void) {}
