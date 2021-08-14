8edde3ed066cd4102ccec09a3c61a7d1cbd9ca3e
Justin Boswell <boswej@amazon.com>
2021-08-14 00:04:39 +0000

    Fixed OS detection/openssl init

--- src/crypto.c
+++ src/crypto.c
@@ -3,6 +3,7 @@
 
 #if defined(AWS_OS_LINUX)
 #    include <openssl/crypto.h>
+#    include <openssl/evp.h>
 #    include <s2n.h>
 
 void aws_crt_crypto_share(void) {
@@ -10,14 +11,17 @@ void aws_crt_crypto_share(void) {
     s2n_crypto_disable_init();
 }
 
+#define AWS_OPENSSL_VERSION_AT_LEAST(major, minor, fix) \
+    (OPENSSL_VERSION_NUMBER >= ((major << 28) + (minor << 20) + (fix << 12)))
+
 void init_crypto(void) {
     /*
      * OpenSSL prior to 1.1.x has idempotency issues with initialization and shutdown.
      * We initialize it minimally ourselves here, since s2n has been told not to.
      * Cleanup is handled by OpenSSL's atexit handler
      */
-#    if !S2N_OPENSSL_VERSION_AT_LEAST(1, 1, 0)
-    OPENSSL_add_all_algorithms();
+#    if !AWS_OPENSSL_VERSION_AT_LEAST(1, 1, 0)
+    OpenSSL_add_all_algorithms();
 #    else
     OPENSSL_init_crypto(
         OPENSSL_INIT_LOAD_CRYPTO_STRINGS | OPENSSL_INIT_ADD_ALL_CIPHERS | OPENSSL_INIT_ADD_ALL_DIGESTS |
@@ -26,7 +30,9 @@ void init_crypto(void) {
 #    endif
 }
 
-void shutdown_crypto(void) {}
+void shutdown_crypto(void) {
+
+}
 
 #else
 void aws_crt_crypto_share(void) {}
